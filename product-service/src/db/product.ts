97311c09e63b317a4eb8f9070cad34a56196f096
Alon Kedar-Haspel <alonkh2@gmail.com>
2024-12-02 21:17:32 +0200

    started implementing products

--- product-service/src/db/product.ts
+++ product-service/src/db/product.ts
@@ -2,10 +2,30 @@ import mongoose, { Schema } from "mongoose";
 
 const Product = new Schema({
 	id: { type: String, required: true },
-	username: { type: String, required: true },
-	email: { type: String, required: true },
 	name: { type: String, required: true },
-	password: { type: String, required: true },
+	price: { type: Number, required: true },
+	items: [
+		{
+			size: { type: Number, required: true },
+			stockAmount: { type: Number, required: true },
+			reserved: [
+				{
+					reservationId: {
+						type: String,
+						required: true,
+					},
+					quantity: {
+						type: Number,
+						required: true,
+					},
+					reservedSince: {
+						type: Date,
+						default: Date.now,
+					},
+				},
+			],
+		},
+	],
 });
 
 export default mongoose.model("Product", Product);
--- product-service/src/index.ts
+++ product-service/src/index.ts
@@ -1,13 +1,41 @@
-import express from 'express';
+import express from "express";
+import product from "./db/product";
+import mongoose from "mongoose";
+import cron from "node-cron";
+
+const cleanUpReservations = async () => {
+	const fifteenMinutesAgo = new Date(Date.now() - 15 * 60 * 1000);
+
+	await product.updateMany(
+		{},
+		{
+			$pull: {
+				"items.$[].reserved": {
+					reservedSince: { $lt: fifteenMinutesAgo },
+				},
+			},
+		}
+	);
+};
 
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send("hello from products")
+app.get("/", (req, res) => {
+	res.send("hello from products");
 });
 
 const PORT = process.env.PRODUCT_PORT || 5003;
 
-app.listen(PORT, () => {
-    console.log(`products service is running on port ${PORT}`)
+mongoose.connect("mongodb://mongo:27017/products").then(() => {
+	console.log("products connected to mongo");
+
+	cron.schedule("* * * * *", cleanUpReservations);
+
+	cleanUpReservations().then(() => {
+		console.log("old reservations removed");
+
+		app.listen(PORT, () => {
+			console.log(`products service is running on port ${PORT}`);
+		});
+	});
 });
